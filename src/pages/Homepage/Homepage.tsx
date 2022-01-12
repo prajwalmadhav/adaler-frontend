0d0ab71d6e34477c580f04c3b5bd56421d1da004
PG <prajwalsmadhav@gmail.com>
2022-01-13 00:01:54 +0530

    Initialized frontend application

---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer';
+import NavBar from '../../components/NavBar/Navbar'
+import Hero from './../../components/Hero/Hero';
+
+
+export default function Homepage() {
+    return (
+        <>
+            <NavBar />
+            <Hero />
+            <Footer />        
+        </>
+    )
+}
