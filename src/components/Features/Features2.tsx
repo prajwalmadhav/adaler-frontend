3f6edaf551e4c13009627e661fd75acaa549838c
Sumukh <sumukhjadhav007@gmail.com>
2022-01-13 21:44:45 +0530

    Made changes to landing page

---
+++ src/components/Features/Features2.tsx
@@ -0,0 +1,59 @@
+import { ReactElement } from 'react';
+import { Box, SimpleGrid, Icon, Text, Stack, Flex } from '@chakra-ui/react';
+import { FcAssistant, FcDonate, FcInTransit } from 'react-icons/fc';
+
+interface FeatureProps {
+  title: string;
+  text: string;
+  icon: ReactElement;
+}
+
+const Feature = ({ title, text, icon }: FeatureProps) => {
+  return (
+    <Stack>
+      <Flex
+        w={16}
+        h={16}
+        align={'center'}
+        justify={'center'}
+        color={'white'}
+        rounded={'full'}
+        bg={'gray.100'}
+        mb={1}>
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{title}</Text>
+      <Text color={'gray.600'}>{text}</Text>
+    </Stack>
+  );
+};
+
+export default function SimpleThreeColumns() {
+  return (
+    <Box p={4} allignment='center'>
+      <SimpleGrid columns={{ base: 10, md: 4 }} spacing={10} allignment="center"> 
+        <Feature
+          icon={<Icon as={FcAssistant} w={10} h={10} />}
+          title={'AI Feedback'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+        <Feature
+          icon={<Icon as={FcDonate} w={10} h={10} />}
+          title={'Free of Cost'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+        <Feature
+          icon={<Icon as={FcInTransit} w={10} h={10} />}
+          title={'Online'}
+          text={
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+          }
+        />
+      </SimpleGrid>
+    </Box>
+  );
+}
--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -2,13 +2,20 @@ import React from 'react'
 import Footer from '../../components/Footer/Footer';
 import NavBar from '../../components/NavBar/Navbar'
 import Hero from './../../components/Hero/Hero';
-
+import Features from './../../components/Features/Features';
+import Features2 from './../../components/Features/Features2';
+import Features3 from './../../components/Features/Features3';
+import Testimonials from './../../components/Testimonials/Testimonials';
 
 export default function Homepage() {
     return (
         <>
             <NavBar />
             <Hero />
+            <Features />
+            <Features2 />
+            <Features3 />
+            <Testimonials />
             <Footer />        
         </>
     )
