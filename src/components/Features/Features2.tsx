fba3653504c260c0acb21bc205be4f4aa03e6989
PG <prajwalsmadhav@gmail.com>
2022-01-15 02:52:39 +0530

    Netlify error resolved by reomoving icons pack

--- src/components/Features/Features2.tsx
+++ src/components/Features/Features2.tsx
@@ -10,11 +10,11 @@ import {
     Icon,
     useColorModeValue,
   } from '@chakra-ui/react';
-  import {
-    IoAnalyticsSharp,
-    IoLogoBitcoin,
-    IoSearchSharp,
-  } from 'react-icons/io5';
+  // import {
+  //   IoAnalyticsSharp,
+  //   IoLogoBitcoin,
+  //   IoSearchSharp,
+  // } from 'react-icons/io5';
   import { ReactElement } from 'react';
   
   interface FeatureProps {
@@ -68,25 +68,25 @@ import {
                   borderColor={useColorModeValue('gray.100', 'gray.700')}
                 />
               }>
-              <Feature
-                icon={
-                  <Icon as={IoAnalyticsSharp} color={'yellow.500'} w={5} h={5} />
-                }
-                iconBg={useColorModeValue('yellow.100', 'yellow.900')}
-                text={'Adaptive Tests'}
-              />
-              <Feature
-                icon={<Icon as={IoLogoBitcoin} color={'green.500'} w={5} h={5} />}
-                iconBg={useColorModeValue('green.100', 'green.900')}
-                text={'Self Paced Learning '}
-              />
-              <Feature
-                icon={
-                  <Icon as={IoSearchSharp} color={'purple.500'} w={5} h={5} />
-                }
-                iconBg={useColorModeValue('purple.100', 'purple.900')}
-                text={'Simple Understanding language'}
-              />
+              {/* <Feature
+              //   icon={
+              //     <Icon as={IoAnalyticsSharp} color={'yellow.500'} w={5} h={5} />
+              //   }
+              //   iconBg={useColorModeValue('yellow.100', 'yellow.900')}
+              //   text={'Adaptive Tests'}
+              // />
+              // <Feature
+              //   icon={<Icon as={IoLogoBitcoin} color={'green.500'} w={5} h={5} />}
+              //   iconBg={useColorModeValue('green.100', 'green.900')}
+              //   text={'Self Paced Learning '}
+              // />
+              // <Feature
+              //   icon={
+              //     <Icon as={IoSearchSharp} color={'purple.500'} w={5} h={5} />
+              //   }
+              //   iconBg={useColorModeValue('purple.100', 'purple.900')}
+              //   text={'Simple Understanding language'}
+              /> */}
             </Stack>
           </Stack>
           <Flex>
