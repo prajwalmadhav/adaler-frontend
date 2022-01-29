b464f1b7db3c7e9580445b80ed95fd40d287ab08
Sumukh <sumukhjadhav007@gmail.com>
2022-01-29 12:51:50 +0530

    Changed Naming Conventions

---
+++ src/components/Modals/SignUpModal.tsx
@@ -0,0 +1,138 @@
+import React, {useRef} from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    FormControl,
+    FormLabel,
+    Input,
+    ChakraProvider,
+    Checkbox,
+    Box,
+    Flex,
+    Text,
+    IconButton,
+    Button,
+    Stack,
+    Collapse,
+    chakra,
+    Icon,
+    Link,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    useColorModeValue,
+    useBreakpointValue,
+    useDisclosure,
+    AlertDescription,
+  } from '@chakra-ui/react';
+  import { Col, Container, Form, Navbar } from "react-bootstrap";
+  import { auth } from "../../firebaseSetup";
+  import { IoMdCheckmarkCircle } from "react-icons/io";
+  
+  export default function SignUpModal() {
+      const emailRef = useRef<HTMLInputElement>(null);
+      const passwordRef = useRef<HTMLInputElement>(null);
+      
+      const createAccount = async () => {
+          try {
+            await auth.createUserWithEmailAndPassword(
+              emailRef.current!.value,
+              passwordRef.current!.value,
+            );
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        const { 
+          isOpen: isOpenReportModal, 
+          onOpen: onOpenReportModal, 
+          onClose: onCloseReportModal 
+      } = useDisclosure();
+        
+        function Alerts(){
+          return (
+            <Flex
+              w="full"
+              bg="gray.600"
+              p={50}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Flex
+                maxW="sm"
+                w="full"
+                mx="auto"
+                bg={useColorModeValue("white", "gray.800")}
+                shadow="md"
+                rounded="lg"
+                overflow="hidden"
+              >
+                <Flex justifyContent="center" alignItems="center" w={12} bg="green.500">
+                  <Icon as={IoMdCheckmarkCircle} color="white" boxSize={6} />
+                </Flex>
+        
+                <Box mx={-3} py={2} px={4}>
+                  <Box mx={3}>
+                    <chakra.span
+                      color={useColorModeValue("green.500", "green.400")}
+                      fontWeight="bold"
+                    >
+                      Success
+                    </chakra.span>
+                    <chakra.p
+                      color={useColorModeValue("gray.600", "gray.200")}
+                      fontSize="sm"
+                    >
+                      Your account was registered!
+                    </chakra.p>
+                  </Box>
+                </Box>
+              </Flex>
+            </Flex>
+          );
+        };
+        
+  return (
+    <ModalContent>
+    <ModalHeader className='ModalHeader'> Sign Up </ModalHeader>
+    <ModalCloseButton/>
+    <ModalBody className='ModalBody'>
+      <Form className="mt-4">
+    <Form.Group controlId="formEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control ref={emailRef} type="email" placeholder="email" />
+    </Form.Group>
+   
+  <Form.Group controlId="formPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control
+      ref={passwordRef}
+      type="password"
+      placeholder="password"
+    />
+  </Form.Group>
+    </Form>
+          <Checkbox>Remember me</Checkbox>
+    </ModalBody>
+    <ModalFooter className='ModalFooter'>
+      <Button colorScheme="blue" mr={160} onClick={() => {
+          createAccount();
+          onCloseReportModal();
+          console.log('lol')
+          Alerts();
+          console.log('lol121')
+        }}> Sign In </Button>
+    </ModalFooter>
+  </ModalContent>
+
+  
+  
+  
+  )
+}
+
--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -39,7 +39,7 @@ import {
   import { auth } from "../../../src/firebaseSetup";
   import { Col, Container, Form, Navbar } from "react-bootstrap";
 import SignInModal from '../Modals/SignInModal';
-import LoginModal from '../Modals/LoginModal';
+import SignUpModal from '../Modals/SignUpModal';
 
   export default function NavBar() {
     
@@ -114,7 +114,7 @@ import LoginModal from '../Modals/LoginModal';
 
             <Modal isOpen={isOpenReportModal} onClose={onCloseReportModal} blockScrollOnMount={false}  isCentered motionPreset='slideInBottom' >
             <ModalOverlay/>
-            <LoginModal />
+            <SignUpModal />
             </Modal>
             </Box>
           </Stack>
