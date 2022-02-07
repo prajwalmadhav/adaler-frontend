8db9ec233ee6745cc271994c2a92a4bd1642e1af
PG <prajwalsmadhav@gmail.com>
2022-02-07 22:44:39 +0530

    App.tsx updated with routing and organized course

--- src/App.tsx
+++ src/App.tsx
@@ -15,15 +15,13 @@ import Courses from './pages/Dashboard/Courses';
 import Sidebar from './components/Dashboard/Sidebar';
 
 import SignUp from './components/LandingPage/Modals/SignUpModal';
-
-import Course1 from './components/Dashboard/Course1';
-import Course2 from './components/Dashboard/Course2';
-import Course3 from './components/Dashboard/Course3';
-import Course4 from './components/Dashboard/Course4';
-import Course5 from './components/Dashboard/Course5';
-import Course6 from './components/Dashboard/Course6';
-import PreQuestions from './pages/PreQuestions';
-
+import Course1 from './components/Dashboard/CourseOverview/Course1';
+import Course2 from './components/Dashboard/CourseOverview/Course2';
+import Course3 from './components/Dashboard/CourseOverview/Course3';
+import Course4 from './components/Dashboard/CourseOverview/Course4';
+import Course5 from './components/Dashboard/CourseOverview/Course5';
+import Course6 from './components/Dashboard/CourseOverview/Course6';
+import Testpg from "./components/Dashboard/Test/Testpg";
 
 
 export const App = () => {
@@ -37,7 +35,6 @@ export const App = () => {
         
         <Routes>
           <Route path="/" element={<Homepage/>}/>
-          <Route path='/Welcome' element={<PreQuestions/>} />
           <Route path='/home' element={<Dashboard/>} />
           <Route path='/smoke' element={<Test/>} />
 
--- src/pages/Dashboard/Courses.tsx
+++ src/pages/Dashboard/Courses.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Route,Link } from 'react-router-dom'
 import Sidebar from '../../components/Dashboard/Sidebar'
 import Home from '../../components/Dashboard/Home'
-import Course1 from '../../components/Dashboard/courseoverview/Course1'
-import Course2 from './../../components/Dashboard/courseoverview/Course2';
-import Course3 from '../../components/Dashboard/courseoverview/Course3'
-import Course4 from '../../components/Dashboard/courseoverview/Course4'
-import Course5 from './../../components/Dashboard/courseoverview/Course5';
-import Course6 from '../../components/Dashboard/courseoverview/Course6'
+import Course1 from '../../components/Dashboard/CourseOverview/Course1'
+import Course2 from './../../components/Dashboard/CourseOverview/Course2';
+import Course3 from '../../components/Dashboard/CourseOverview/Course3'
+import Course4 from '../../components/Dashboard/CourseOverview/Course4'
+import Course5 from './../../components/Dashboard/CourseOverview/Course5';
+import Course6 from '../../components/Dashboard/CourseOverview/Course6'
 import { Divider } from '@chakra-ui/react'
 
 
