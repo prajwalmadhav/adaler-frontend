bed90f3e148059575e8763f431c3b3b913d071ec
Trupthi <trupthimuralidhar13@gmail.com>
2022-02-07 18:33:57 +0530

    SignUp and Prequestions page

---
+++ src/pages/PreQuestions.tsx
@@ -0,0 +1,43 @@
+import React, {useRef} from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    FormControl,
+    FormLabel,
+    Input,
+    ChakraProvider,
+    Checkbox,
+    Box,
+    Flex,
+    Text,
+    IconButton,
+    Stack,
+    Collapse,
+    Icon,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    useColorModeValue,
+    useBreakpointValue,
+    useDisclosure,
+    useToast,
+  } from '@chakra-ui/react';
+  import { Button, Col, Container, Form, Navbar } from "react-bootstrap";
+
+
+export default function PreQuestions() {
+    
+  
+  return (
+    <Button className='Welcome'>
+        Welcome
+
+    </Button> 
+   
+  )
+}
