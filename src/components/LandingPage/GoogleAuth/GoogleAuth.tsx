0e6f4b695540c80ee5e9e21fe70d5e62c0d503ff
Sumukh <sumukhjadhav007@gmail.com>
2022-03-04 11:02:55 +0530

    Fixed google auth bug

--- src/components/LandingPage/GoogleAuth/GoogleAuth.tsx
+++ src/components/LandingPage/GoogleAuth/GoogleAuth.tsx
@@ -6,13 +6,13 @@ import firebase from 'firebase/compat/app';
 
 const GoogleAuth = async(navigate: NavigateFunction, setIsLoadingGoogle: React.Dispatch<React.SetStateAction<boolean>>) => {
   const user = firebase.auth().currentUser;  
-  auth.signInWithPopup(provider).then(async () => {
+  await auth.signInWithPopup(provider).then(async () => {
       if (auth.currentUser?.metadata?.creationTime !== auth.currentUser?.metadata?.lastSignInTime)
           {
             navigate('/home');
           }
           else{
-            const name = user?.displayName
+            const name = await user?.displayName
             const email = user?.email
             const uid = user?.uid
             const ref = firebase.firestore().collection("person")
