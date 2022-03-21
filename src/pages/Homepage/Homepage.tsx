8c2f483ec8beb768119d1fd146979ea9514ed731
PG <prajwalsmadhav@gmail.com>
2022-03-22 03:06:47 +0530

    updated call pages for landing page

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import Footer from '../../components/LandingPage/Footer/Footer';
 import NavBar from '../../components/LandingPage/NavBar/Navbar'
 import Hero from './../../components/LandingPage/Hero/Hero';
-import Features1 from './../../components/LandingPage/Features/Features1';
 import Features2 from '../../components/LandingPage/Features/Features2';
 import Features3 from '../../components/LandingPage/Features/Features3';
-import Features4 from '../../components/LandingPage/Features/Features4';
 import Testimonials from './../../components/LandingPage/Testimonials/Testimonials';
-import { BrowserRouter as Router, Route,Link } from 'react-router-dom'
+import Features5 from '../../components/LandingPage/Features/Features5';
 
 export default function Homepage() {
     return (
@@ -18,8 +15,12 @@ export default function Homepage() {
             {/* <Features1 /> */}
             <Features2 />
             <Features3 />
+            {/* <Features4 /> */}
+            <Features5 />
             <Testimonials />
-            <Footer />        
+            <Footer />
+            
+
         </>
     )
 }
