5fee2f5dffe69ca3916f827cfaaf97078891b6ce
PG <prajwalsmadhav@gmail.com>
2022-03-30 16:17:06 +0530

    Carousel update

--- src/components/Dashboard/Carousels.tsx
+++ src/components/Dashboard/Carousels.tsx
@@ -41,23 +41,23 @@ export default function CaptionCarousel() {
   // This can be static or loaded from a server
   const cards = [
     {
-      title: 'Design Projects 1',
+      title: '',
       text:
-        "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+        "",
       image:
         'https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
     },
     {
-      title: 'Design Projects 2',
+      title: '',
       text:
-        "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+        "",
       image:
         'https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80',
     },
     {
-      title: 'Design Projects 3',
+      title: '',
       text:
-        "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
+        "",
       image:
         'https://images.unsplash.com/photo-1507237998874-b4d52d1dd655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
     },
@@ -68,7 +68,7 @@ export default function CaptionCarousel() {
       ml= {{ base:0, lg:'16%'}}
       mb= {{ base:3, lg:'4%'}}
       position={'relative'}
-      height={{ base:'400px', lg:'600px'}}
+      height={{ base:'400px', lg:'400px'}}
       width={{ base:'full', lg:'82%'}}
       overflow={'hidden'}>
       {/* CSS files for react-slick */}
